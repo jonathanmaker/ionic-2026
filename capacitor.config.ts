7d6fe215e9905722ceefa315bddc792fdbad2d51
Jonathan Damian <jonathantbm2015@gmail.com>
2026-01-04 10:25:10 -0300

    primer commit plantilla en blanco

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'entrega.ionic.v2026',
+  appName: 'entrega-ionic-2026',
+  webDir: 'www'
+};
+
+export default config;
